6a05a720a1e8ee42a88ca261a83f55186c3b81b9
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2024-04-12 16:54:06 +0530

    initial-commit

---
+++ src/Room.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { appId } from "./components/helper";
+import { serversecret } from "./components/helper";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    const appID = appId;
+    const serverSecret = serversecret;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Ganiya"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Copy Link",
+          url: `http://localhost:3000/room/${roomID}`,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
---
+++ src/components/helper.js
@@ -0,0 +1,2 @@
+export const appId = 1445023235;
+export const serversecret = "ea9a403541104025ea6e5a1c2c50c8d9";
