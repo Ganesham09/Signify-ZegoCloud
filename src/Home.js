36602bfc2db71863c89c885e3981b96aabdf045c
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2024-04-14 00:26:28 +0530

    detdetailing

--- src/Home.js
+++ src/Home.js
@@ -32,7 +32,9 @@ const Home = () => {
             <h1 className="text-[50px] md:text-[80px] text-white font-bold pt-12">
               Signify Video Chat
             </h1>
-            <p className="text-[26px] text-white  -mt-2">With ZegoCloud</p>
+            <p className="text-[26px] text-white  -mt-2">
+              Bridging Silence, Building Bonds
+            </p>
           </div>
 
           {/* Enter Code */}
